f7225b8f0ac399027cbbcc23fd8eca59908ad94d
Arvind Venkatesan <varvind@tamu.edu>
2021-03-05 10:54:18 -0600

    Added a few changes for rubocop to pass and removed commented code

--- spec/dashboard_spec.rb
+++ spec/dashboard_spec.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
 RSpec.describe 'Dashboard Page', type: :system do
   describe 'Check' do
     before(:each) do
@@ -6,20 +10,19 @@ RSpec.describe 'Dashboard Page', type: :system do
       fill_in('admin_user[password]', with: 'password')
       click_button('Login')
     end
-	
-	
+
     it 'for capitalization' do
       visit '/admin/dashboard'
-      sleep(5)
       expect(page).to have_content('BMES')
     end
-	
-	it 'for text' do
+
+    it 'for text' do
       visit '/admin/dashboard'
-      sleep(5)
       expect(page).to have_content('Welcome to the BMES Officer Area!')
-	  expect(page).to have_content('Our organization is committed to Advancing Human Health and Well Being')
-	  expect(page).to have_content('To get started, select the \'Events\' tab from the Navigation Bar to create a new event, or select \'Participations\' to view member sign ins!')
+      expect(page).to have_content('Our organization is committed to Advancing Human Health and Well Being')
+      # rubocop:disable Layout/LineLength
+      expect(page).to have_content('To get started, select the \'Events\' tab from the Navigation Bar to create a new event, or select \'Participations\' to view member sign ins!')
+      # rubocop:enable Layout/LineLength
     end
   end
 end
